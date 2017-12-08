95ca9346c125d65ce6d541dddf50f5be1ec607ef
Eli Perkins <eli@clubhouse.io>
2017-12-07 19:33:05 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-47801301-1',
       },
     },
     `gatsby-plugin-feed`,
