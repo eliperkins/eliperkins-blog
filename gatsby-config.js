ae0bb4497d9318ea2032768d2cf20c85724d2d2b
Eli Perkins <eli.j.perkins@gmail.com>
2019-03-22 18:00:28 -0400

    Configure production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,8 +103,7 @@ module.exports = {
             {
               allMarkdownRemark(
                 limit: 1000,
-                sort: { order: DESC, fields: [frontmatter___date] },
-                filter: {frontmatter: { draft: { ne: true } }}
+                sort: { order: DESC, fields: [frontmatter___date] }
               ) {
                 edges {
                   node {
