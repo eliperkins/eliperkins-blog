da73e5e0ba1d6784d49e1d6f371be8b76bee2882
Eli Perkins <eli.j.perkins@gmail.com>
2024-04-25 13:33:03 -0600

    Clean up styling more

--- components/bio.tsx
+++ components/bio.tsx
@@ -1,32 +1,31 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import profilePic from './headshot.jpg';
 
 function Bio() {
   return (
-    <div className='flex items-center'>
+    <div className="flex items-center">
       <Image
         src={profilePic}
         alt="Eli Perkins"
-        className='w-16 h-16 rounded-full mr-2'
-        style={{
-          // marginRight: rhythm(1 / 2),
-          // marginBottom: 0,
-          // width: rhythm(2),
-          // height: rhythm(2),
-          // borderRadius: '50%'
-        }}
+        className="w-16 h-16 rounded-full mr-2"
       />
       <p>
-        Written by <span className='font-semibold'>Eli Perkins</span>, a mobile
-        engineer based in Denver.{' '}
-        <a className='underline text-amber-600 font-semibold hover:text-amber-700' href="https://mastodon.online/@eliperkins">
+        Written by{' '}
+        <Link href="https://eliperkins.com" className="font-semibold underline underline-offset-2">
+          Eli Perkins
+        </Link>
+        , a mobile engineer based in Denver.{' '}
+        <a
+          className="underline underline-offset-2 text-amber-600 font-semibold hover:text-amber-700"
+          href="https://mastodon.online/@eliperkins"
+        >
           Say hello on Mastodon.
         </a>
       </p>
     </div>
   );
 }
-
 
 export default Bio;
